40350bce31d30b8755af15a0ebff610b68cc4937
hchairi <hchairi1902@gmail.com>
2024-11-24 09:41:46 +0100

    register function

--- todoprj/todoapp/urls.py
+++ todoprj/todoapp/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from django.contrib.auth.views import LogoutView
+from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='home-page'),
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', views.register, name='register'),
+    path('login/', views.login, name='login'),
 ]
--- todoprj/todoapp/views.py
+++ todoprj/todoapp/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
+def home(request):
+    return render(request, 'todoapp/todo.html', {})
+
+
+def register(request):
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        if len(password) < 3:
+            messages.error(request, "Password must be at least 3 characters")
+            return redirect('register')
+        
+        get_all_users_by_username = User.objects.filter(username=username)
+        
+        if get_all_users_by_username:
+            messages.error(request, 'Error: Username already exists, use another')
+            return redirect('register') 
+        
+        new_user = User.objects.create_user(username=username, email=email, password=password)
+        new_user.save()
+        
+    return render(request, 'todoapp/register.html', {})
+
+
+def login(request):
+    return render(request, 'todoapp/login.html', {})
